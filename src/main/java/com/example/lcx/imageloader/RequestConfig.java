77550eb97f07c645ed9c650bf445981ce5a8fa4a
LcxAndroidwawa <525603977@qq.com>
2016-03-19 03:29:35 +0800

    delete

--- src/main/java/com/example/lcx/imageloader/RequestConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.lcx.imageloader;
-
-/**
- * Created by Administrator on 2016/3/19 0019.
- */
-public class RequestConfig {
-    public static final String  meizi="http://f.hiphotos.baidu.com/image/pic/item/dbb44aed2e738bd4f60e4017a48b87d6277ff9ed.jpg";
-}
